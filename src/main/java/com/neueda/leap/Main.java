76e1cc15f47de7d4df93dce971e5fd6a71b1770b
sromy17 <45542891+sromy17@users.noreply.github.com>
2026-08-24 19:59:45 +0000

    Update greeting message in Main.java

--- src/main/java/com/neueda/leap/Main.java
+++ src/main/java/com/neueda/leap/Main.java
@@ -1,6 +1,6 @@
 package com.neueda.leap;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world from the team's Sprint 2 project skeleton");
+        System.out.println("Hello world from blah blah blah ");
     }
 }
