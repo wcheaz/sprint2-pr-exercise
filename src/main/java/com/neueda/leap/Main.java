704c03cd7ebb86ea6045f903ef6874f5207244f8
Matthew Chai <matthewalexanderchai@gmail.com>
2026-08-25 13:18:43 +0000

    feat: Added hi message

--- src/main/java/com/neueda/leap/Main.java
+++ src/main/java/com/neueda/leap/Main.java
@@ -2,5 +2,6 @@ package com.neueda.leap;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world from blah blah blah blah again");
+	System.out.println("Hi.");
     }
 }
